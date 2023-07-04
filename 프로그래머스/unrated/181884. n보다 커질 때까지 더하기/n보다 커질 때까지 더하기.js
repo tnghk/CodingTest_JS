4783dfdf767413a29f19526cf23107743053f23d
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2023-07-04 23:28:53 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,10 @@
+function solution(numbers, n) {
+    let answer = 0;
+    let idx = 0;
+    
+    while(answer <= n) {
+        answer += numbers[idx];
+        idx++;
+    }
+    return answer;
+}
