2a21e3c3e2fdf2a2d6f8fb9fa66be7bc6b6bf122
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2023-08-10 19:04:14 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,14 +1,14 @@
 function solution(s) {
     let answer = '';
     let idx = 0;
-    
+
     for(let i = 0; i < s.length; i++) {
         if(s[i] === " ") {
             idx = 0;
             answer += " ";
             continue;
         }
-        
+
         answer += idx % 2 ? s[i].toLowerCase() : s[i].toUpperCase();
         idx++;
     }
