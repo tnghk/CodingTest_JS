167e4538ea60fbead4d0520a9d646f6a3aa60072
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2023-08-23 22:21:26 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,5 +1,3 @@
 function solution(my_string, overwrite_string, s) {
-    let string = [...my_string];
-    string.splice(s, overwrite_string.length, overwrite_string);
-    return string.join('');
+    return my_string.slice(0, s) + overwrite_string + my_string.slice(s + overwrite_string.length);
 }
