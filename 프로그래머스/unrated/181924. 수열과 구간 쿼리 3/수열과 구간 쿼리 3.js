b5dbd4e4b55e57bb418fd49e0d32949de2994759
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2023-08-29 13:56:11 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.41 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,8 +1,6 @@
 function solution(arr, queries) {
-    for(let i = 0; i < queries.length; i++) {
-        let num = arr[queries[i][0]];
-        arr.splice(queries[i][0], 1, arr[queries[i][1]]);
-        arr.splice(queries[i][1], 1, num);
-    }
+    queries.forEach( ([a,b]) => {
+        [arr[a],arr[b]] = [arr[b],arr[a]];
+    })
     return arr;
 }
