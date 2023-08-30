72e10eec789f0397450656b2d652f4be0378fb25
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2023-08-30 20:45:07 +0900

    [unrated] Title: 배열 만들기 2, Time: 16.93 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,8 +1,4 @@
 function solution(l, r) {
-    let answer = [];
-    
-    for(let i = l; i <= r; i++) {
-        if(i.toString().replaceAll(/[05]/g, '') === '') answer.push(i);
-    }
-    return answer.length ? answer : [-1];
+    const result = Array.from({length:r-l+1}, (_,i)=>i+l).filter(n=>!/[^05]/.test(n));
+    return result.length ? result : [-1];
 }
