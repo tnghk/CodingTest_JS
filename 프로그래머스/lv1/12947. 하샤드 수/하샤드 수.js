8ffdabcbc6a742e57a6b0e770ad5f732a6a3c813
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2023-09-06 14:58:38 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,10 +1,4 @@
 function solution(x) {
-    let num = x;
-    let sum = 0;
-    do {
-        sum += x%10;
-        x = Math.floor(x/10);
-    } while (x>0);
-
-    return !(num%sum);
+    const sum = (x + '').split('').reduce((acc, cur) => acc + Number(cur), 0);
+    return !(x % sum);
 }
