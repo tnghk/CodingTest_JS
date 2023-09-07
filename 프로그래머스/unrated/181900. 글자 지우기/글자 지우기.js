c710d17f644f44a855a06351e480a7b18b112f5d
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2023-09-08 01:30:57 +0900

    [unrated] Title: 글자 지우기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
@@ -1,6 +1,16 @@
+// function solution(my_string, indices) {
+//     let answer = '';
+    
+//     for(let i of indices) {
+        
+//     }
+// }
+
+
+
 function solution(my_string, indices) {
     return indices.sort((a, b) => b - a).reduce((str, cur) => {
-        str.splice(cur, 1);
+        str[cur] = '';
         return str;
     }, [...my_string]).join('');
 }
