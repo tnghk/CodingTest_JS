1e388c95b8cd04d05da44f3facc08b28fbecfdb3
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2023-09-08 03:03:39 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,14 +1,25 @@
-function solution(str_list) {
-    const lIdx = str_list.indexOf("l");
-    const rIdx = str_list.indexOf("r");
+// function solution(str_list) {
+//     const lIdx = str_list.indexOf("l");
+//     const rIdx = str_list.indexOf("r");
     
-    if(lIdx === -1 || rIdx === -1){
-        if(lIdx > -1)   return str_list.slice(0, lIdx);         // l만 있을 경우
-        else if(rIdx > -1)  return str_list.slice(rIdx + 1);    // r만 있을 경우
+//     if(lIdx === -1 || rIdx === -1){
+//         if(lIdx > -1)   return str_list.slice(0, lIdx);         // l만 있을 경우
+//         else if(rIdx > -1)  return str_list.slice(rIdx + 1);    // r만 있을 경우
         
-        return [];  // l과 r 둘 다 없을 경우
-    } 
+//         return [];  // l과 r 둘 다 없을 경우
+//     } 
     
-    // l과 r 둘 다 있을 경우
-    return lIdx < rIdx ? str_list.slice(0, lIdx) : str_list.slice(rIdx + 1);
+//     // l과 r 둘 다 있을 경우
+//     return lIdx < rIdx ? str_list.slice(0, lIdx) : str_list.slice(rIdx + 1);
+// }
+
+
+function solution(str_list) {
+    
+    for(let i = 0; i < str_list.length; i++) {
+        if(str_list[i] === "l")    return str_list.slice(0, i);
+        if(str_list[i] === "r")    return str_list.slice(i + 1);
+    }
+    
+    return [];
 }
