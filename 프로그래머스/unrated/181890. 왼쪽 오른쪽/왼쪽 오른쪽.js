a064902471c4279ed6d9e09e6af9f090f1474821
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2023-09-08 02:57:12 +0900

    [unrated] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,13 +1,14 @@
 function solution(str_list) {
     const lIdx = str_list.indexOf("l");
-    const rIdx = str_list.indexOf("r")
+    const rIdx = str_list.indexOf("r");
+    
     if(lIdx === -1 || rIdx === -1){
-        if(lIdx > -1)   return str_list.slice(0, lIdx);
-        else if(rIdx > -1)  return str_list.slice(rIdx + 1);
-        else return [];
-    } else if(lIdx < rIdx) {
-        return str_list.slice(0, lIdx);
-    } else {
-        return str_list.slice(rIdx + 1);
-    }
+        if(lIdx > -1)   return str_list.slice(0, lIdx);         // l만 있을 경우
+        else if(rIdx > -1)  return str_list.slice(rIdx + 1);    // r만 있을 경우
+        
+        return [];  // l과 r 둘 다 없을 경우
+    } 
+    
+    // l과 r 둘 다 있을 경우
+    return lIdx < rIdx ? str_list.slice(0, lIdx) : str_list.slice(rIdx + 1);
 }
