286586e620ed6d1b462c6f23caaa549c34a4cff0
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2023-09-08 23:14:52 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,19 +1,14 @@
 function solution(num) {
     let cnt = 0;
     
+    if(num === 1)   return 0;
+    
     while(num !== 1) {
-        if(cnt >= 500){
-            cnt = -1;
-            break;
-        }
+        if(cnt === 500) return -1;
         
-        if(num % 2 === 0) {
-            num = num / 2;
-        } else {
-            num = num * 3 + 1;
-        }
-        
+        num = num % 2 === 0 ? num / 2 : num * 3 + 1;
         cnt++;
     }
+
     return cnt;
 }
