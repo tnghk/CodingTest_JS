b9cb87405df3925b9e1d6264912ab4f7843d4762
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2023-09-13 01:20:55 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,12 +1,3 @@
 function solution(absolutes, signs) {
-    let answer = 0;
-    
-    for(let i = 0; i < signs.length; i++) {
-        if(signs[i]){
-            answer += absolutes[i];
-        } else {
-            answer -= absolutes[i];
-        }
-    }
-    return answer;
+    return absolutes.reduce((acc, v, i) => signs[i] ? acc + v : acc - v, 0);
 }
