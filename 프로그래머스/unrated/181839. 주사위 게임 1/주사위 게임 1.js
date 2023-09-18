2847fbd0096b4d464452ce1145c5b3b0e95518cb
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2023-09-19 02:46:03 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -1,10 +1,8 @@
 function solution(a, b) {
-
-    if(a % 2 !== 0 && b % 2 !== 0) {
-        return a * a + b * b;
-    } else if(a % 2 !== 0 || b % 2 !== 0) {
-        return 2 * (a + b);
-    } else {
-        return Math.abs(a - b)
+    if((a + b) % 2 === 0) {
+        if(a % 2 === 1) return a**2 + b**2;
+        return Math.abs(a - b);
     }
+    
+    return 2 * (a + b);
 }
