4028e2e797b17988c3c068546552289103bd7c30
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2023-09-20 18:30:20 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,9 +1,11 @@
 function solution(rank, attendance) {
     const [ a, b, c ] = rank.reduce((arr, cur, i) => {
-        if(attendance[i])
+        if(attendance[i])   //rank 각 원소의 대회 참여가 가능할 경우
             arr[cur - 1] = i;
         return arr;
     }, []).filter(v => v >= 0).slice(0, 3);
     
     return 10000 * a + 100 * b + c;
 }
+
+
