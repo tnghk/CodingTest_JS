bfffaca866906d092af9e81d410ab0d8bd9e2470
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2023-12-27 22:43:12 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 116 ms, Memory: 9372 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -0,0 +1,10 @@
+let fs = require("fs");
+let input = fs.readFileSync("dev/stdin").toString();
+
+let answer = '';
+
+for(let i = 1; i <= input; i++) {
+    answer += `${' '.repeat(input - i)}${'*'.repeat(i)} \n`;
+}
+
+console.log(answer);
