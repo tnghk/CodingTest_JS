2afda72b24d32e976b02e8391b9f288a56b07e29
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2023-12-27 22:31:18 +0900

    [Bronze V] Title: A+B - 7, Time: 116 ms, Memory: 9364 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,11 @@
+let fs = require("fs");
+let input = fs.readFileSync("dev/stdin").toString().split("\n");
+
+let answer = '';
+
+for(let i = 1; i <= input[0]; i++) {
+    let [a, b] = input[i].split(' ').map(Number);
+    answer += `Case #${i}: ${a + b} \n`;
+}
+
+console.log(answer);
