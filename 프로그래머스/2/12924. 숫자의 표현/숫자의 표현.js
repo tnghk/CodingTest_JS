458cf9248152f972edec8d3b6192230a27aefeb5
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2024-03-05 21:50:02 +0900

    [level 2] Title: 숫자의 표현, Time: 2.28 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -2,7 +2,7 @@ function solution(n) {
     let sum = 0;
     let cnt = 1;
     
-    for(let i = 1; i < Math.ceil(n / 2); i++) {
+    for(let i = 1; i < n / 2; i++) {
         sum = 0;
 
         for(let j = i; sum < n; j++) {
