d8d36fe465daa00f03024783a8388165cc64063e
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2024-03-24 23:33:20 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 136 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.js
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.js
@@ -2,8 +2,6 @@ let fs = require("fs");
 let input = fs.readFileSync("dev/stdin").toString().trim();
 let alphabet = Array(26).fill(0);
 
-for(let i = 0; i < input.length; i++) {
-    alphabet[input.charCodeAt(i) - 97] += 1;
-}
+[...input].map((_, i) => alphabet[input.charCodeAt(i) - 97] += 1)
 
 console.log(alphabet.join(' '));
