a23b62c54eab756c7c26ad1684387e21cb368b7a
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2024-03-30 23:49:32 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 672 ms, Memory: 68724 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -0,0 +1,18 @@
+let fs = require("fs");
+let input = fs.readFileSync("dev/stdin").toString().trim().split('\n');
+let pos = input.slice(1).map(v => v.split(' ').map(Number));
+
+pos.sort((a, b) => {
+    let [a1, a2] = a;
+    let [b1, b2] = b;
+
+    if(a1 < b1) return -1;
+    else if(a1 === b1) {
+        if(a2 < b2) return -1;
+        else if(a2 > b2) return 1;
+        else return 0;
+    } 
+    else return 1;
+});
+
+console.log(pos.map(v => v.join(' ')).join('\n'));
