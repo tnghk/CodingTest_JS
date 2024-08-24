2730203905912a41d97c6f71f269b02de1becec9
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2024-08-25 00:12:48 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 716 ms, Memory: 128516 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -1,15 +1,15 @@
 let fs = require("fs");
 let input = fs.readFileSync("dev/stdin").toString().trim().split('\n');
-let cnt = {};
+let cntMap = new Map();
 
 input[1].split(' ').forEach(num => {
-  if(!cnt[num]) {
-    cnt[num] = 1;
+  if(!cntMap.has(num)) {
+    cntMap.set(num, 1);
   } else {
-    cnt[num]++;
+    let cnt = cntMap.get(num);
+    cntMap.set(num, cnt + 1);
   }
 });
 
-let answer = input[3].split(' ').map(num => cnt[num] ? cnt[num] : 0);
-
+let answer = input[3].split(' ').map(num => cntMap.has(num) ? cntMap.get(num) : 0);
 console.log(answer.join(' '));
