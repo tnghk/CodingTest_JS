dcc6f750a8504dc3246c7fbb87d389178ce54a06
yoonsoohwa <48947282+yoonsoohwa@users.noreply.github.com>
2024-08-27 14:36:43 +0900

    [level 2] Title: 예상 대진표, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,10 @@
+function solution(n,a,b) {
+    let round = 1;
+    while(true) {
+        a = Math.ceil(a / 2);
+        b = Math.ceil(b / 2);
+        
+        if(a === b) return round;
+        round++;
+    }
+}
